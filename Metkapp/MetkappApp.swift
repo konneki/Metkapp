8d31f930cd37c834dceec4ccade2327c4e0fdd0f
Konneki <konnekdaniel@gmail.com>
2023-06-24 10:17:23 +0200

    Initial Commit

---
+++ Metkapp/MetkappApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetkappApp.swift
+//  Metkapp
+//
+//  Created by Daniel Konnek on 24.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct MetkappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
